e39a28abb815822a19aa1c161e716cb8b649b512
Sarvani Sanaboyina <80517819+sarvani1997@users.noreply.github.com>
2022-07-11 16:53:04 +0530

    update projects in portfolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,26 +1,26 @@
 import "./Portfolio.css";
 
 import myJournal from "./images/myjournal.png";
-import craneMaintenace from "./images/craneMaintenace.png";
-import sunriseEggsFarm from "./images/sunriseEggsFarm.png";
+import issueTracker from "./images/issue-tracker.png";
 import reddit from "./images/reddit.png";
 import periodTracker from "./images/periodTracker.png";
 import whileIEat from "./images/whileIEat.png";
 
 let work = [
   {
-    screenshot: myJournal,
-    title: "My Journal App",
-    desciption: "A web app that lets users to note down thier daily work.",
-    link: "https://journal-cf0q.onrender.com/",
-    git: "https://github.com/sarvani1997/journal-app",
+    screenshot: issueTracker,
+    title: "Issue Tracker",
+    desciption: "clone of github issues",
+    link: "https://issue-tracker-nu.vercel.app/",
+    git: "https://github.com/sarvani1997/issue_tracker",
   },
   {
-    screenshot: periodTracker,
-    title: "Period  Tracker",
-    desciption: "Here, the user can track and predict their period.",
-    link: "https://period-tracker.onrender.com/",
-    git: "https://github.com/sarvani1997/period-tracker",
+    screenshot: whileIEat,
+    title: "While I Eat",
+    desciption:
+      "This web application is to view popular movies and tv shows and their streamimg platform.",
+    link: "https://while-i-eat.onrender.com/",
+    git: "https://github.com/sarvani1997/WhileIEat",
   },
   {
     screenshot: reddit,
@@ -30,26 +30,18 @@ let work = [
     git: "https://github.com/sarvani1997/reddit",
   },
   {
-    screenshot: craneMaintenace,
-    title: "Crane Maintenance",
-    desciption: "To manage crane operation details and log their records.",
-    link: "https://jai-work.onrender.com/",
+    screenshot: periodTracker,
+    title: "Period  Tracker",
+    desciption: "Here, the user can track and predict their period.",
+    link: "https://period-tracker.onrender.com/",
+    git: "https://github.com/sarvani1997/period-tracker",
   },
   {
-    screenshot: sunriseEggsFarm,
-    title: "Sunrise Eggs Farm",
-    desciption:
-      "This web application is to record and manage payment details in the farm near Odissa.",
-    link: "https://sun-rise-egg-farm.onrender.com/",
-    git: "https://github.com/sarvani1997/sunshine_egg_farm",
-  },
-  {
-    screenshot: whileIEat,
-    title: "While I Eat",
-    desciption:
-      "This web application is to view popular movies and tv shows and their streamimg platform.",
-    link: "https://while-i-eat.onrender.com/",
-    git: "https://github.com/sarvani1997/WhileIEat",
+    screenshot: myJournal,
+    title: "My Journal App",
+    desciption: "A web app that lets users to note down thier daily work.",
+    link: "https://journal-cf0q.onrender.com/",
+    git: "https://github.com/sarvani1997/journal-app",
   },
 ];
 
@@ -65,17 +57,24 @@ function Porfolio() {
                 <div className="overlap">
                   <img src={w.screenshot} alt={w.title} />
                   <div className="redirect-btns">
-                    {w.git && (
-                      <a href={w.git} target="_blank" className="btn-link">
-                        Git Repo
-                      </a>
-                    )}
+                    <a href={w.git} target="_blank" className="btn-link">
+                      Source Code
+                    </a>
+
                     <a href={w.link} target="_blank" className="btn-link">
                       View Website
                     </a>
                   </div>
                 </div>
                 <h3 className="title">{w.title}</h3>
+                <div className="links">
+                  <a className="btn-link-2" href={w.git}>
+                    Source Code
+                  </a>
+                  <a className="btn-link-2" href={w.link}>
+                    Website
+                  </a>
+                </div>
               </div>
             );
           })}
